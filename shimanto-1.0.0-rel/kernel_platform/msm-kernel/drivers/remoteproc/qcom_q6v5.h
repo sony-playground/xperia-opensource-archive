30496da614a3feaf74a1b8719f1d2240cf5783b3
BladeRunner-A2C <creator4983@aospa.co>
2025-06-18 15:40:37 +0200

    Open source archive for 71.0.A.2.22
    Open source download for Xperia 1 VII (XQ-FS72, SO-51F, SOG15, A501SO, XQ-FS44, XQ-FS54);
    software version 71.0.A.2.22
    
    https://developer.sony.com/file/download/open-source-archive-for-71-0-a-2-22

---
+++ shimanto-1.0.0-rel/kernel_platform/msm-kernel/drivers/remoteproc/qcom_q6v5.h
@@ -0,0 +1,75 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * NOTE: This file has been modified by Sony Corporation.
+ * Modifications are Copyright 2021 Sony Corporation,
+ * and licensed under the license of the file.
+ */
+
+#ifndef __QCOM_Q6V5_H__
+#define __QCOM_Q6V5_H__
+
+#include <linux/kernel.h>
+#include <linux/completion.h>
+#include <linux/soc/qcom/qcom_aoss.h>
+
+struct icc_path;
+struct rproc;
+struct qcom_smem_state;
+struct qcom_sysmon;
+
+#define SUBSYS_CRASH_REASON_LEN 512
+
+struct qcom_q6v5 {
+	struct device *dev;
+	struct rproc *rproc;
+
+	struct qcom_smem_state *state;
+	struct qmp *qmp;
+
+	struct icc_path *path;
+
+	unsigned stop_bit;
+
+	int wdog_irq;
+	int fatal_irq;
+	int ready_irq;
+	int handover_irq;
+	int stop_irq;
+
+	struct rproc_subdev *ssr_subdev;
+
+	struct work_struct crash_handler;
+
+	bool handover_issued;
+
+	struct completion start_done;
+	struct completion stop_done;
+
+	int crash_reason;
+	int crash_stack;
+	unsigned int smem_host_id;
+	char crash_reason_buf[SUBSYS_CRASH_REASON_LEN];
+	int data_ready;
+
+	bool running;
+
+	const char *load_state;
+	void (*handover)(struct qcom_q6v5 *q6v5);
+	unsigned long long seq;
+	unsigned long long crash_seq;
+};
+
+int qcom_q6v5_init(struct qcom_q6v5 *q6v5, struct platform_device *pdev,
+		   struct rproc *rproc, int crash_reason, int crash_stack,
+		   unsigned int smem_host_id, const char *load_state,
+		   void (*handover)(struct qcom_q6v5 *q6v5));
+void qcom_q6v5_deinit(struct qcom_q6v5 *q6v5);
+
+void qcom_q6v5_register_ssr_subdev(struct qcom_q6v5 *q6v5, struct rproc_subdev *ssr_subdev);
+int qcom_q6v5_prepare(struct qcom_q6v5 *q6v5);
+int qcom_q6v5_unprepare(struct qcom_q6v5 *q6v5);
+int qcom_q6v5_request_stop(struct qcom_q6v5 *q6v5, struct qcom_sysmon *sysmon);
+int qcom_q6v5_wait_for_start(struct qcom_q6v5 *q6v5, int timeout);
+unsigned long qcom_q6v5_panic(struct qcom_q6v5 *q6v5);
+
+#endif
